5237c836607d5401f81e6c9d9dfca2d539315d6e
Andrei <andryushik@gmail.com>
2023-04-19 19:49:16 +0200

    added get, delete, update, like post API

--- server/src/controllers/post.js
+++ server/src/controllers/post.js
@@ -14,6 +14,19 @@ export const getPosts = async (req, res) => {
   }
 };
 
+export const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json({ success: true, result: post });
+  } catch (error) {
+    logError(error);
+    res.status(500).json({
+      success: false,
+      msg: `Unable to get post with id: ${req.params.id}`,
+    });
+  }
+};
+
 export const createPost = async (req, res) => {
   try {
     const { post } = req.body;
@@ -45,5 +58,79 @@ export const createPost = async (req, res) => {
     res
       .status(500)
       .json({ success: false, msg: "Unable to create post, try again later" });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json({
+        success: true,
+        msg: "The post has been updated",
+      });
+    } else {
+      res
+        .status(403)
+        .json({ success: false, msg: "You can update only your post" });
+    }
+  } catch (error) {
+    logError(error);
+    res.status(500).json({
+      success: false,
+      msg: "Unable to update post. Error: " + error,
+    });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json({
+        success: true,
+        msg: "The post has been deleted",
+      });
+    } else {
+      res
+        .status(403)
+        .json({ success: false, msg: "You can delete only your post" });
+    }
+  } catch (error) {
+    logError(error);
+    res.status(500).json({
+      success: false,
+      msg: "Unable to delete post. Error: " + error,
+    });
+  }
+};
+
+export const likePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json({
+        success: true,
+        msg: "The post has been liked",
+      });
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json({
+        success: true,
+        msg: "The post has been disliked",
+      });
+    }
+  } catch (error) {
+    logError(error);
+    res.status(500).json({
+      success: false,
+      msg: "Unable to like/dislike post. Error: " + error,
+    });
   }
 };
--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -1,9 +1,20 @@
 import express from "express";
-import { createPost, getPosts } from "../controllers/post.js";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  getPosts,
+  likePost,
+  updatePost,
+} from "../controllers/post.js";
 
 const postRouter = express.Router();
 
-postRouter.get("/", getPosts);
-postRouter.post("/create", createPost);
+postRouter.get("/", getPosts); // get all posts //get timeline posts
+postRouter.get("/:id", getPost); //get a post
+postRouter.post("/create", createPost); // create a new post
+postRouter.put("/:id", updatePost); // update a post
+postRouter.delete("/:id", deletePost); //delete a post
+postRouter.put("/:id/like", likePost); //like/dislike a post
 
 export default postRouter;
