2e4467d81ac97a81bca8ddc3e1d676973547293e
Huzthepro <hzyfakcay@gmail.com>
2023-04-24 13:14:35 +0200

    Fix: Small changes for my post page

--- client/src/components/AddNewPost.jsx
+++ client/src/components/AddNewPost.jsx
@@ -1,32 +1,16 @@
-import React, { useState } from "react";
-import "./AddNewPost.css";
-import "../components/Button";
-import postBackground from "../assets/post-background.png";
-import Button from "../components/Button";
-import NewPostModal from "../components/NewPostModal";
-// import PropTypes from "prop-types";
+import React from "react";
+import PropTypes from "prop-types";
 
-const AddNewPost = () => {
-  const [visible, setVisible] = useState(false);
-  const buttonFunction = () => {
-    setVisible(true);
-  };
-  const closeModal = () => {
-    setVisible(false);
-  };
+const AddNewPost = ({ setActive }) => {
   return (
     <div className="add-new-post">
-      {visible && <NewPostModal closeModal={closeModal} />}
-      <img src={postBackground} alt="user background" />
-      <div className="post-button">
-        <Button buttonFunction={buttonFunction} buttonContent={"+ Add Post"} />
-      </div>
+      <h1 onClick={() => setActive(false)}>Hi</h1>
     </div>
   );
 };
 
-// Input.propTypes = {
-//   name: PropTypes.string.isRequired,
-// };
+AddNewPost.propTypes = {
+  setActive: PropTypes.func.isRequired,
+};
 
 export default AddNewPost;
