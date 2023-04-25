9ae57b8d608d776d143387d719176bef2d0ffa40
Huzthepro <hzyfakcay@gmail.com>
2023-04-25 11:31:37 +0200

    Single Post+ Profile picture + Navbar fix

--- client/src/components/MyPostsRight.jsx
+++ client/src/components/MyPostsRight.jsx
@@ -1,31 +1,15 @@
 import React from "react";
-import Loading from "./Loading";
+
+import FriendsList from "./FriendsList";
 const MyPostsRight = () => {
-  const loading = true;
-  const error = "This is an error example";
   return (
     <div className="right-section">
       <div className="triple-container">
-        <div className="top-div has-loading">
-          {error && <div className="error">{error}</div>}
-          {loading && (
-            <Loading loadMessage={"Loading will blur the background"} />
-          )}
-        </div>
+        <div className="top-div has-loading">This will be Calendar</div>
         <div className="middle-div">
-          -Error and loading is: only in the parts they are relative. Not the
-          whole page
-          <br />
-          <br />
-          -If some div has loading in it: It must have the class has-loading
-          <br />
-          <br />
-          {error && <div className="error">{error}</div>}
+          <FriendsList />
         </div>
-        <div className="bottom-div has-loading">
-          {error && <div className="error">{error}</div>}
-          {loading && <Loading />}
-        </div>
+        <div className="bottom-div has-loading">This will be popular tags</div>
       </div>
     </div>
   );
---
+++ client/src/components/ProfilePicture.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import profileIcon from "../assets/profile-icon.png";
+import PropTypes from "prop-types";
+import "./ProfilePicture.css";
+
+//Size: "":13vw | "medium":8.5vw | "small":6.5vw | "smaller":4.5vw
+//Border: "":37px | "square":0px | "circle":50%
+const ProfilePicture = ({ profilePicture, size, border }) => {
+  return (
+    <div className={`profile-picture-component ${size}`}>
+      <img
+        className={border}
+        src={profilePicture ? profilePicture : profileIcon}
+        alt="profile picture"
+      />
+    </div>
+  );
+};
+// This required for Eslint, without this: { props } make a problem.
+ProfilePicture.propTypes = {
+  profilePicture: PropTypes.string,
+  size: PropTypes.string,
+  border: PropTypes.string,
+};
+
+export default ProfilePicture;
