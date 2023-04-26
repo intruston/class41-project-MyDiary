83f30e2039ff9ebf1580fa618cba189a386ec66e
Huzthepro <hzyfakcay@gmail.com>
2023-04-26 17:50:54 +0200

    Fix: making single post component more dry

--- client/src/components/SinglePost.jsx
+++ client/src/components/SinglePost.jsx
@@ -1,71 +1,35 @@
-import React, { useContext, useEffect, useState, useRef } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import useGetAnotherUser from "../hooks/useGetAnotherUser";
-import useFetch from "../hooks/useFetch";
+
+import PostDate from "./PostDate";
+import PostReaction from "./PostReaction";
 import ProfilePicture from "./ProfilePicture";
 import Loading from "./Loading";
 import "./SinglePost.css";
-import { UserContext } from "../hooks/useUserContext";
 
-import PropTypes from "prop-types";
-
+//Use this for mapped post or single post. Sending post alone is enough. It takes required info from the post itself and make required fetch operations.
 const SinglePost = ({ mappedPost }) => {
-  const { user } = useContext(UserContext);
-  const date = new Date(mappedPost.createdAt);
-  const options = { month: "long", day: "numeric" };
-  const formattedDate = date.toLocaleDateString("en-US", options);
-  const [likes, setLikes] = useState(mappedPost.likes);
-  const isLikedByUser = likes.includes(user._id);
-  const {
-    isLoading: anotherUserLoading,
-    error: anotherUserError,
-    anotherUser,
-  } = useGetAnotherUser({
+  //Taking User information from post
+  const { isLoading, error, anotherUser } = useGetAnotherUser({
     anotherUserId: mappedPost.userId,
   });
-  const { error, performFetch, cancelFetch } = useFetch(
-    `/post/${mappedPost._id}/like`,
-    (response) => {
-      setLikes(response.result);
-      if (heartIconRef.current) {
-        isLikedByUser
-          ? heartIconRef.current.classList.remove("is-animating")
-          : heartIconRef.current.classList.add("is-animating");
-      }
-    }
-  );
-  useEffect(() => {
-    return cancelFetch;
-  }, []);
-
-  const heartIconRef = useRef(null);
-
-  const likePost = () => {
-    performFetch({
-      method: "PUT",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        userId: user._id,
-      }),
-    });
-  };
 
   return (
     <>
-      <div className="post-date">
-        <hr />
-        <h3>{formattedDate}</h3>
-        <hr />
-      </div>
+      {/* Date */}
+      <PostDate date={mappedPost.createdAt} />
+
+      {/* Post */}
       <div className="pos-container">
         <div className="side-profile has-loading">
-          {anotherUserLoading && <Loading />}
+          {isLoading && <Loading />}
           <ProfilePicture
             profilePicture={anotherUser ? anotherUser.profilePicture : null}
             size={"small"}
           />
         </div>
+
         <div className="post-content">
           <div className="post-header">
             <div className="inside-profile">
@@ -75,25 +39,16 @@ const SinglePost = ({ mappedPost }) => {
               />
               <h3>{anotherUser ? anotherUser.firstName : null}</h3>
             </div>
-
             <h2>...</h2>
           </div>
+
           <p>{mappedPost.content}</p>
+          {error && <div className="error">{error.message}</div>}
         </div>
       </div>
-      <div className="post-reaction" onClick={likePost}>
-        <div
-          style={{ backgroundImage: "url(/heart-animation.png)" }}
-          className={`heart-icon ${isLikedByUser ? "pink" : "grey"}`}
-          ref={heartIconRef}
-        ></div>
-        <div className="likes">{likes.length}</div>
-      </div>
-      {(error || anotherUserError) && (
-        <div className="error">
-          {error.message}||{anotherUserError.message}{" "}
-        </div>
-      )}
+
+      {/* Post reactions */}
+      <PostReaction id={mappedPost._id} totalLikes={mappedPost.likes} />
     </>
   );
 };
