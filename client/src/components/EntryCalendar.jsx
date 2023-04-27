c6e1f6ec754525b9c7e39bed4f2c2b703b2fd33f
Yusuf Elik <ysfelik1@gmail.com>
2023-04-27 23:22:12 +0200

    compare post dates from context

--- client/src/components/EntryCalendar.jsx
+++ client/src/components/EntryCalendar.jsx
@@ -1,47 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "./EntryCalendar.css";
 import { postDatesContext } from "../hooks/usePostDatesContext";
 import { useContext } from "react";
 
+import { UserContext } from "../hooks/useUserContext";
+import useFetch from "../hooks/useFetch";
+import Loading from "./Loading";
+
 const EntryCalendar = () => {
   const [value, onChange] = useState(new Date());
-  const myPostList = [
-    "11.4.2023",
-    "12.4.2023",
-    "9.4.2023",
-    "24.4.2023",
-    "20.5.2023",
-    "17.3.2023",
-  ];
 
+  //getting post data
+  const { user } = useContext(UserContext);
+  const [posts, setPosts] = useState([]);
+  // const [activeTab, setActiveTab] = useState("public");
+  const { isLoading, error, performFetch, cancelFetch } = useFetch(
+    `/post/timeline/${user._id}`,
+    (response) => {
+      setPosts(response.result);
+    }
+  );
+  useEffect(() => {
+    performFetch();
+    return cancelFetch;
+  }, []);
+
+  useEffect(() => {}, [posts]);
+
+  // eslint-disable-next-line no-console
+  console.log(posts);
   const { setDate } = useContext(postDatesContext);
-
+  //set value to selected day
   const handleDateChange = (value) => {
     onChange(value);
     setDate(value);
   };
-  const tileClassName = ({ date }) => {
-    const realDate = `${date.getDate()}.${
-      date.getMonth() + 1
-    }.${date.getFullYear()}`;
-    if (myPostList.includes(realDate)) {
-      return "highlight";
-    }
-  };
 
-  return (
-    <div className="calendar-container">
-      <Calendar
-        calendarClassName="my-calendar"
-        onChange={handleDateChange}
-        value={value}
-        tileClassName={tileClassName}
-        locale="en"
-      />
-    </div>
-  );
+  //highlight dates of posts
+  if (posts) {
+    // const tileClassName = ({ date }) => {
+    //   const realDate = `${date.getDate()}.${
+    //     date.getMonth() + 1
+    //   }.${date.getFullYear()}`;
+    //   const hasPost = posts.some((post) => post.createdAt === realDate);
+    //   return hasPost ? "highlight" : null;
+    // };
+    const tileClassName = ({ date }) => {
+      const postDates = posts.map((post) => post.createdAt.substr(0, 10));
+      const dateString = date.toISOString().substr(0, 10);
+      const hasPost = postDates.includes(dateString);
+      return hasPost ? "highlight" : null;
+    };
+
+    return (
+      <div className="calendar-container">
+        {isLoading && <Loading />}
+        <Calendar
+          calendarClassName="my-calendar"
+          onChange={handleDateChange}
+          value={value}
+          tileClassName={tileClassName}
+          locale="en"
+        />
+        {error && <div className="error">{error.message}</div>}
+      </div>
+    );
+  }
 };
 
 export default EntryCalendar;
