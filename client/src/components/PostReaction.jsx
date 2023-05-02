7c477782d5f69ae42edce62d914a8c4d606510c9
Huzthepro <hzyfakcay@gmail.com>
2023-05-02 10:56:01 +0200

    Dry reaction

--- client/src/components/PostReaction.jsx
+++ client/src/components/PostReaction.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { useUserContext } from "../hooks/useUserContext";
 import useFetch from "../hooks/useFetch";
 import PropTypes from "prop-types";
@@ -8,18 +8,12 @@ const PostReaction = ({ id, totalLikes }) => {
   const { user } = useUserContext();
   const [likes, setLikes] = useState(totalLikes);
   const isLikedByUser = likes.includes(user._id);
-  const heartIconRef = useRef(null);
 
   //Fetch for likes
   const { error, performFetch, cancelFetch } = useFetch(
     `/post/${id}/like`,
     (response) => {
       setLikes(response.result);
-      if (heartIconRef.current) {
-        isLikedByUser
-          ? heartIconRef.current.classList.remove("is-animating")
-          : heartIconRef.current.classList.add("is-animating");
-      }
     }
   );
   useEffect(() => {
@@ -39,8 +33,7 @@ const PostReaction = ({ id, totalLikes }) => {
     <div className="post-reaction" onClick={likePost}>
       <div
         style={{ backgroundImage: "url(/heart-animation.png)" }}
-        className={`heart-icon ${isLikedByUser ? "pink" : "grey"}`}
-        ref={heartIconRef}
+        className={`heart-icon ${isLikedByUser ? "pink is-animating" : "grey"}`}
       ></div>
       <div className="likes">{likes.length}</div>
       {error && <div className="error">{error.message}</div>}
