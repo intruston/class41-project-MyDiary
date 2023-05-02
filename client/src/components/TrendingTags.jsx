956094bd5ab6c5fd2bd0c74bb88088afb8e3bb71
Yusuf Elik <ysfelik1@gmail.com>
2023-05-03 00:50:31 +0200

    create TrendingTags component

---
+++ client/src/components/TrendingTags.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./trendingTags.css";
+const TrendingTags = () => {
+  return (
+    <div className="tags-container">
+      <div className="tags">
+        <p>one</p>
+      </div>
+    </div>
+  );
+};
+
+export default TrendingTags;
