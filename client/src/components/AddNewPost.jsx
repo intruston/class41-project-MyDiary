64dda32832a51c8bc1773795de3b3be402464407
Huzthepro <hzyfakcay@gmail.com>
2023-05-03 15:58:43 +0200

    Add post design

--- client/src/components/AddNewPost.jsx
+++ client/src/components/AddNewPost.jsx
@@ -108,8 +108,18 @@ const AddNewPost = ({ setActive }) => {
               Private
             </h3>
           </div>
-          <div className="new-post-bottom-right ">
-            <div className="new-post friends">{icons.friends}</div>
+          <div className="new-post-bottom-middle">
+            {/* {fethed.image && ( */}
+            <div className="post-image">
+              <img
+                src="https://cdn.pixabay.com/photo/2022/03/09/14/11/cat-7057971_960_720.png"
+                alt="post image"
+                onError={(e) => (e.target.src = "")}
+              ></img>
+            </div>
+            {/* )} */}
+          </div>
+          <div className="new-post-bottom-right">
             <div className="new-post attach"> {icons.attach}</div>
 
             <button type="submit" className="post-publish-button">
