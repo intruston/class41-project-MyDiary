21682ccc8d15477ba05a6871b479e73fd5fb2832
Yusuf Elik <ysfelik1@gmail.com>
2023-05-03 02:57:46 +0200

    create popular tags contreller

--- client/src/components/TrendingTags.jsx
+++ client/src/components/TrendingTags.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./trendingTags.css";
+import { Link } from "react-router-dom";
 const TrendingTags = () => {
+  const tags = ["downtown", "long", "bike", "say", "way"];
+
   return (
     <div className="tags-container">
-      <div className="tags">
-        <p>one</p>
-      </div>
+      {tags.map((tag) => (
+        <div key={tag} className="tags">
+          <Link to={`/search/tags?q=${tag}`}>
+            <p>{tag}</p>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
--- server/src/controllers/search.js
+++ server/src/controllers/search.js
@@ -38,3 +38,54 @@ export const searchTags = async (req, res) => {
     });
   }
 };
+export const mostPopularTags = async (req, res) => {
+  try {
+    const most = parseInt(req.params.most); // Convert string to number
+    const searchQuery = await Post.aggregate([
+      // Match documents with non-empty tags array and isPrivate: false and isBanned: false
+      {
+        $match: {
+          tags: { $exists: true, $ne: [] },
+          isPrivate: false,
+          isBanned: false,
+        },
+      },
+
+      // Group documents by tag and count their occurrences
+      { $unwind: "$tags" },
+      { $group: { _id: "$tags", count: { $sum: 1 } } },
+
+      // Sort documents by count in descending order
+      { $sort: { count: -1 } },
+
+      // Facet stage to get the top N tags and their total count
+      {
+        $facet: {
+          // First pipeline gets the top tags
+          topTags: [
+            { $limit: most },
+            { $project: { _id: 0, tag: "$_id", count: 1 } },
+          ],
+          // Second pipeline gets the total count of tags
+          totalCount: [{ $count: "total" }],
+        },
+      },
+
+      // Project the top tags and total count
+      {
+        $project: {
+          topTags: "$topTags",
+          total: { $arrayElemAt: ["$totalCount.total", 0] },
+        },
+      },
+    ]);
+
+    res.status(200).json({ success: true, result: searchQuery });
+  } catch (error) {
+    logError(error);
+    res.status(500).json({
+      success: false,
+      msg: `Unable to find posts, error: ${error}`,
+    });
+  }
+};
