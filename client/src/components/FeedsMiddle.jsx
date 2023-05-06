347ebde3039b30f8de8a6ab849512332b0a0fe81
Huzthepro <hzyfakcay@gmail.com>
2023-05-06 16:34:58 +0200

    Feed page search bar

--- client/src/components/FeedsMiddle.jsx
+++ client/src/components/FeedsMiddle.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import SinglePost from "./SinglePost";
 import useFetch from "../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 import Loading from "./Loading";
+import SearchIcon from "@mui/icons-material/Search";
 import { useUserContext } from "../hooks/useUserContext";
 
 const FeedsMiddle = () => {
   // Getting user information and logout function from context
   const { user } = useUserContext();
   const [posts, setPosts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
 
   const { isLoading, error, performFetch, cancelFetch } = useFetch(
     `/post/feed/${user._id}`,
@@ -29,6 +33,19 @@ const FeedsMiddle = () => {
     return !post.isPrivate && !post.isBanned;
   });
 
+  //Handle Search
+  const sanitizeTags = (value) => {
+    let sanitizedValue = value.trim(); // Remove leading and trailing spaces
+    sanitizedValue = sanitizedValue.replace(/^[#\s]+/, ""); // Remove '#' symbols and spaces from the beginning
+    return sanitizedValue;
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const sanitizedTags = sanitizeTags(searchQuery);
+    sanitizedTags
+      ? navigate(`/search/tags/${sanitizedTags}`)
+      : navigate("/search");
+  };
   return (
     <div className="middle-section">
       <div className="middle-container ">
@@ -38,7 +55,20 @@ const FeedsMiddle = () => {
             <h2>Feed</h2>
           </div>
           <div className="right">
-            <h3>Search</h3>
+            {/* Search Bar */}
+            <form onSubmit={handleSubmit} className="feed-search-form">
+              <input
+                type="text"
+                placeholder="Search posts"
+                minLength={2}
+                value={searchQuery}
+                onChange={(event) => setSearchQuery(event.target.value)}
+                className="feed-search-input"
+              />
+              <button type="submit" className="feed-search-button">
+                <SearchIcon className="feed-search-icon" />
+              </button>
+            </form>
           </div>
         </div>
         {error && <div className="error">{error.message}</div>}
